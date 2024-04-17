9781cc7943f78aa096e53c9372b4dce12b941911
Danniel Navas <dannielnavas@gmail.com>
2024-04-17 08:00:04 -0500

    ✨ Actualizar manejo de eventos en CreateTodoButton.jsx y TodoSearch.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import CreateTodoButton from "./components/CreateTodoButton";
 import TodoCounter from "./components/TodoCounter";
@@ -6,20 +7,33 @@ import TodoList from "./components/TodoList";
 import TodoSearch from "./components/TodoSearch";
 
 function App() {
-  const completed = 10;
-  const total = 30;
-  const defaultTodos = [
+  const [searchValue, setSearchValue] = useState("");
+
+  console.log(searchValue);
+
+  const [todos, setTodos] = useState([
     { text: "Cortar cebolla", completed: false },
     { text: "Tomar el curso de intro a React", completed: true },
     { text: "Llorrar con la Llorona", completed: false },
     { text: "Llamar", completed: false },
-  ];
+  ]);
+
+  const completed = todos.filter((todo) => !!todo.completed).length;
+  const total = todos.length;
+
+  const searchedTodo = todos.filter((todo) => {
+    const textTodo = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+
+    return textTodo.includes(searchText);
+  });
+
   return (
     <>
       <TodoCounter completed={completed} total={total} />
-      <TodoSearch />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {defaultTodos.map((todo, index) => (
+        {searchedTodo.map((todo, index) => (
           <TodoItems key={index} name={todo.text} completed={todo.completed} />
         ))}
       </TodoList>
