4e4042bfe1218ede2f74cf2a6c9bcea716ad0a1d
Danniel Navas <dannielnavas@gmail.com>
2024-04-18 07:48:59 -0500

    ✨ Actualizar manejo de eventos en CreateTodoButton.jsx y TodoSearch.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,13 +28,35 @@ function App() {
     return textTodo.includes(searchText);
   });
 
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text); // Encuentra el index del todo que se quiere completar
+    const newTodos = [...todos]; // Copia el array de todos
+    newTodos[todoIndex].completed = true; // Cambia el valor de completed a true
+    setTodos(newTodos); // Actualiza el estado de todos
+  };
+
+  const deleteTodo = (text) => {
+    // const todoIndex = todos.findIndex((todo) => todo.text === text); // Encuentra el index del todo que se quiere eliminar
+    // const newTodos = [...todos]; // Copia el array de todos
+    // newTodos.splice(todoIndex, 1); // Elimina el todo del array
+
+    const newTodos = todos.filter((todo) => todo.text !== text); // Filtra los todos que no sean el que se quiere eliminar
+    setTodos(newTodos); // Actualiza el estado de todos
+  };
+
   return (
     <>
       <TodoCounter completed={completed} total={total} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
         {searchedTodo.map((todo, index) => (
-          <TodoItems key={index} name={todo.text} completed={todo.completed} />
+          <TodoItems
+            key={index}
+            name={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
 
