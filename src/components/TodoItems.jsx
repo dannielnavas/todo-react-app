88c7a3a213608c287a8bc9f846840024aa98a140
Danniel Navas <dannielnavas@gmail.com>
2024-04-18 07:49:11 -0500

    ✨ Actualizar manejo de eventos en TodoItems.jsx

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -2,14 +2,19 @@
 
 import "./TodoItem.css";
 
-const TodoItems = ({ name, completed }) => {
+const TodoItems = ({ name, completed, onComplete, onDelete }) => {
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>
+      <span
+        className={`Icon Icon-check ${completed && "Icon-check--active"}`}
+        onClick={onComplete}
+      >
         V
       </span>
       <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>{name}</p>
-      <span className="Icon Icon-delete">X</span>
+      <span className="Icon Icon-delete" onClick={onDelete}>
+        X
+      </span>
     </li>
   );
 };
