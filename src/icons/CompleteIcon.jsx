17aa649292a77da8e2f37211a6323b70c85d969b
Danniel Navas <dannielnavas@gmail.com>
2024-04-23 07:40:25 -0500

    ✨ Check Icon

---
+++ src/icons/CompleteIcon.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+
+import { TodoIcon } from "./TodoIcon";
+
+const CompleteIcon = ({ completed, onComplete }) => {
+  return (
+    <TodoIcon
+      type="check"
+      color={completed ? "green" : "gray"}
+      onClick={onComplete}
+    />
+  );
+};
+
+export default CompleteIcon;
