9feba142d8c377666ce47b967b12926153f35b91
Danniel Navas <dannielnavas@gmail.com>
2024-05-09 07:47:20 -0500

    ✨ feat: Add functionality to submit new TODO in TodoForm

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,16 +1,39 @@
+import { useContext, useState } from "react";
+import { TodoContext } from "./../../TodoContext";
 import "./TodoForm.css";
 
 const TodoForm = () => {
-  const submit = (event) => {
+  const { setOpenModal, addTodo } = useContext(TodoContext);
+  const [newTodoValue, setNewTodoValue] = useState("");
+
+  const onSubmit = (event) => {
     event.preventDefault();
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  };
+
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
   };
 
   return (
-    <form onSubmit={submit(event)}>
+    <form onSubmit={onSubmit}>
       <label>Esribe tu nuevo TODO</label>
-      <textarea placeholder="Hacer tareas" />
+      <textarea
+        placeholder="Hacer tareas"
+        value={newTodoValue}
+        onChange={onChange}
+      />
       <div className="TodoForm-buttonContainer">
-        <button type="button" className="TodoForm-button TodoForm-button--cancel">
+        <button
+          type="button"
+          className="TodoForm-button TodoForm-button--cancel"
+          onClick={onCancel}
+        >
           Cancelar
         </button>
         <button type="submit" className="TodoForm-button TodoForm-button--add">
