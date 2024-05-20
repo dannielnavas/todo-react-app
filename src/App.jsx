b195f27d0a5199467eaadeb1bd391d25447afebb
Danniel Navas <dannielnavas@gmail.com>
2024-05-20 07:41:40 -0500

    ✨ feat: Update TodoCounter and TodoSearch components
    This commit updates the TodoCounter and TodoSearch components to accept props for completed, total, searchValue, and setSearchValue. This improves integration with the TodoContext and provides more flexibility in managing todos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,64 @@
 import "./App.css";
 
-import AppUI from "./AppUI";
-import { TodoProvider } from "./TodoContext";
+import CreateTodoButton from "./components/CreateTodo";
+import EmptyTodos from "./components/EmptyTodos";
+import Modal from "./components/Modal";
+import TodoCounter from "./components/TodoCounter";
+import TodoForm from "./components/TodoForm";
+import TodoItems from "./components/TodoItems";
+import TodoList from "./components/TodoList";
+import TodoSearch from "./components/TodoSearch";
+import TodosError from "./components/TodosError";
+import TodosLoading from "./components/TodosLoading";
+import { useTodos } from "./hook/useTodos";
 
 function App() {
+  const {
+    error,
+    loading,
+    searchedTodo,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    total,
+    completed,
+    searchValue,
+    setSearchValue,
+    addTodo,
+  } = useTodos();
+
+  console.log(searchedTodo);
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoCounter completed={completed} total={total} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+
+      <div>
+        {error && <TodosError error={error} />}
+        {loading && <TodosLoading />}
+        {!loading && !searchedTodo.length && <EmptyTodos />}
+        <TodoList>
+          {searchedTodo?.map((todo) => (
+            <TodoItems
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
+      </div>
+
+      <CreateTodoButton setOpenModal={setOpenModal} />
+
+      {openModal && (
+        <Modal>
+          <TodoForm setOpenModal={setOpenModal} addTodo={addTodo} />
+        </Modal>
+      )}
+    </>
   );
 }
 
