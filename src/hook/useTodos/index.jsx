b99fc949ae1cfda408affb3ecfd8af6760511d7c
Danniel Navas <dannielnavas@gmail.com>
2024-05-20 07:42:30 -0500

    ✨ feat: Agregar hook useTodos para gestionar la lógica de los todos
    Este commit agrega el hook useTodos, que se encarga de gestionar la lógica de los todos. El hook incluye funciones para completar, eliminar y agregar nuevos todos, así como también para realizar búsquedas en la lista de todos. Además, se utiliza el hook useLocalStorage para almacenar los todos en el localStorage.

---
+++ src/hook/useTodos/index.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
+import { useState } from "react";
+import useLocalStorage from "../useLocalStorage";
+
+const useTodos = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const {
+    items: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_V1", []);
+
+  const total = todos.length;
+  const completed = todos.filter((todo) => !!todo.completed).length;
+  const searchedTodo = todos.filter((todo) => {
+    const textTodo = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+
+    return textTodo.includes(searchText);
+  });
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text); // Encuentra el index del todo que se quiere completar
+    const newTodos = [...todos]; // Copia el array de todos
+    newTodos[todoIndex].completed = true; // Cambia el valor de completed a true
+    // setTodos(newTodos); // Actualiza el estado de todos
+    saveTodos(todos); // Actualiza el localStorage
+  };
+
+  const deleteTodo = (text) => {
+    // const todoIndex = todos.findIndex((todo) => todo.text === text); // Encuentra el index del todo que se quiere eliminar
+    // const newTodos = [...todos]; // Copia el array de todos
+    // newTodos.splice(todoIndex, 1); // Elimina el todo del array
+
+    const newTodos = todos.filter((todo) => todo.text !== text); // Filtra los todos que no sean el que se quiere eliminar
+    // setTodos(newTodos); // Actualiza el estado de todos
+    saveTodos(newTodos); // Actualiza el localStorage
+  };
+
+  const addTodo = (text) => {
+    const newTodos = [...todos]; // Copia el array de todos
+    newTodos.push({
+      text,
+      completed: false,
+    });
+    saveTodos(newTodos); // Actualiza el localStorage
+  };
+
+  return {
+    total,
+    completed,
+    searchValue,
+    setSearchValue,
+    searchedTodo,
+    completeTodo,
+    deleteTodo,
+    loading,
+    error,
+    openModal,
+    setOpenModal,
+    addTodo,
+  };
+};
+
+export { useTodos };
