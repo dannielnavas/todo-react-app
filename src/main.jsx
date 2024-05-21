8178977ce19e720b0e155d425d3efe4bacd38884
Danniel Navas <dannielnavas@gmail.com>
2024-05-21 07:58:22 -0500

    feat: Update main.jsx to HOC hight order components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/* eslint-disable react/prop-types */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// function App(props) {
+//   return (
+//     <h1>
+//       ¡{props.saludo}, {props.nombre}!
+//     </h1>
+//   );
+// }
+
+// function withSaludo(WrappedComponent) {
+//   return function WrappedComponentWithSaludo(saludo) {
+//     return function component(props) {
+//       return (
+//         <>
+//           <WrappedComponent {...props} saludo={saludo} />
+//           <p>Hola</p>
+//         </>
+//       );
+//     };
+//   };
+// }
+
+// const AppWithSaludo = withSaludo(App)("Hey");
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <AppWithSaludo nombre="Danniel" />
+//   </React.StrictMode>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
